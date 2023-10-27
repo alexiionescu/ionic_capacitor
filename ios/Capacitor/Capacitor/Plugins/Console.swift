a8f0617c180eeb6f4c2fcb3f9f66243ec5807e89
Alex Johnson <alexiionescu@gmail.com>
2023-10-27 10:46:51 +0000

    custom logs

--- ios/Capacitor/Capacitor/Plugins/Console.swift
+++ ios/Capacitor/Capacitor/Plugins/Console.swift
@@ -2,10 +2,22 @@ import Foundation
 
 @objc(CAPConsolePlugin)
 public class CAPConsolePlugin: CAPPlugin {
+    let log = FileLogger(subsystem: "capacitor", category: "CAPConsole")
 
     @objc public func log(_ call: CAPPluginCall) {
         let message = call.getString("message") ?? ""
         let level = call.getString("level") ?? "log"
-        CAPLog.print("⚡️  [\(level)] - \(message)")
+        switch level {
+        case "log", "info":
+            self.log.info(message)
+        case "error":
+            self.log.error(message)
+        case "warn":
+            self.log.warning(message)
+        default:
+            self.log.debug(message)
+            break
+        }
+        // CAPLog.print("⚡️  [\(level)] - \(message)")
     }
 }
